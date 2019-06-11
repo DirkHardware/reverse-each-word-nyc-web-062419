05848d4ffeab9ef2be780adc95064a496a5a9845
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 21:03:00 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+#
+def reverse_each_word(sentence)
+  revarray = []
+  sentarray = sentence.split(" ")
+  sentarray.collect do |word|
+    revword = word.reverse
+    revarray << revword
+  end
+  revarray.join(" ")
+end
+
+# def reverse_each_word(sentence)
+#   sentarray = sentence.split(" ")
+#   sentarray.each do |word|
+#     word.reverse
+#   end
+#   sentarray.join(" ")
+# end
