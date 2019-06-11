82dee63087a5a7a1020367fbf99bccabce7a4ec7
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 20:52:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,8 +11,18 @@
 #   revarray.join("")
 # end
 
+# def reverse_each_word(sentence)
+#   sentarray = sentence.split("")
+#   revarray = sentarray.reverse
+#   revsent = revarray.join
+# end 
+
 def reverse_each_word(sentence)
-  sentarray = sentence.split("")
-  revarray = sentarray.reverse
-  revsent = revarray.join
-end 
+  revarray = []
+  sentarray = sentence.split(" ")
+  sentarray.each do |word|
+    revword = word.reverse
+    revarray < revword 
+  end 
+  revarray.join("")
+end
